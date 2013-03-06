3153878e96a571faa9fdff80ffc8e6e2fe005d21
Sparky0815 <christianld@web.de>
2013-03-06 14:14:02 +0100

    add light sensor TSL2561

--- arduino_wetterstation/arduino_wetterstation.ino
+++ arduino_wetterstation/arduino_wetterstation.ino
@@ -5,6 +5,8 @@
 #include <DHT.h>
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561.h>
 
 // MAC address for your ethernet shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -22,6 +24,7 @@ DHT dht(DHTPIN, DHTTYPE);
 DHT dht_2(DHTPIN_2, DHTTYPE);
 
 Adafruit_BMP085 bmp;
+Adafruit_TSL2561 tsl = Adafruit_TSL2561(TSL2561_ADDR_FLOAT, 12345);
 
 // Define the strings for our datastream IDs
 char sensorId[] = "innenTemperatur";
@@ -30,6 +33,7 @@ char bufferId[] = "innenLuftfeuchte";
 char outTempId[] = "aussenTemperatur";
 char outHumidityId[] = "aussenLuftfeuchte";
 char pressureId[] = "Luftdruck";
+char lightId[] = "Helligkeit";
 
   CosmDatastream datastreams[] = {
   CosmDatastream(sensorId, strlen(sensorId), DATASTREAM_FLOAT),
@@ -38,13 +42,60 @@ char pressureId[] = "Luftdruck";
   CosmDatastream(outTempId, strlen(outTempId), DATASTREAM_FLOAT),
   CosmDatastream(outHumidityId, strlen(outHumidityId), DATASTREAM_FLOAT),
   CosmDatastream(pressureId, strlen(pressureId), DATASTREAM_FLOAT),
+  CosmDatastream(lightId, strlen(lightId), DATASTREAM_FLOAT),
 };
 
 // Finally, wrap the datastreams into a feed
-CosmFeed feed(101375, datastreams, 5 /* number of datastreams */); // replace INSERT_OUR_FEED_ID with your feed id
+CosmFeed feed(101375, datastreams, 6 /* number of datastreams */); // replace INSERT_OUR_FEED_ID with your feed id
 
 EthernetClient client;
 CosmClient cosmclient(client);
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2561
+*/
+/**************************************************************************/
+void configureSensor(void)
+{
+  /* You can also manually set the gain or enable auto-gain support */
+  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
+  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
+  tsl.enableAutoGain(true);          /* Auto-gain ... switches automatically between 1x and 16x */
+  
+  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+
+  /* Update these values depending on what you've set above! */  
+  Serial.println("------------------------------------");
+  Serial.print  ("Gain:         "); Serial.println("Auto");
+  Serial.print  ("Timing:       "); Serial.println("13 ms");
+  Serial.println("------------------------------------");
+}
 
 void setup() {
    
@@ -66,9 +117,45 @@ void setup() {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
+  
+  Serial.println("Light Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!tsl.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+  
+  /* We're ready to go! */
+  Serial.println("");
+  
 }
 
 void loop() {
+  
+  
+   sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    Serial.print(event.light); Serial.println(" lux");
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
   
 // get temperature from dht22; put to datastream and serial-monitor
   float sensorValue = dht.readTemperature();
@@ -112,6 +199,12 @@ void loop() {
   datastreams[4].setFloat(sensorValue5);
   Serial.print("Read sensor value ");
   Serial.println(datastreams[4].getFloat());  
+  
+// get temperature from dht22; put to datastream and serial-monitor
+  float sensorValue6 = event.light;
+  datastreams[5].setFloat(sensorValue6);
+  Serial.print("Read sensor value ");
+  Serial.println(datastreams[5].getFloat());
     
 
 
